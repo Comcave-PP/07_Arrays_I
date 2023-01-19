bb48b633eec416bb14eb73a16c16d9ee9d34b3bf
Marinus <marinus.fehling@web.de>
2023-01-19 11:55:34 +0100

    Erste schritte mit Arrays

--- script_01.js
+++ script_01.js
@@ -2,28 +2,42 @@
 
 /* Die Satzbau-Maschine | Arrays */
 
-/*** 02. Theorie: Array */
-
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 
-output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin", "der","coole","Max","Mütze"));
 
-function getSentence(word1, word2, word3, word4)
-{
-    const GAP = " ";
-    const PUNCT = ".";
+// function getSentence()
+// {
+//     const GAP = " ";
+//     const PUNCT = ".";
 
-    let str = word1 + GAP +
-              word2 + GAP +
-              word3 + GAP +
-              word4 +
-              PUNCT;
-              
-    return str;
-}
+//     let str = word1 + GAP +
+//               word2 + GAP +
+//               word3 + GAP +
+//               word4 + GAP +
+//               word5 + GAP +
+//               word6 +
+//               PUNCT;
 
+//     return str;
+// }
 
+/*** 02. Theorie: Array */
+
+let arr;
+// arr = new Array();  // Konstruktor
+arr = []; // Literal
+arr = [2,6,74,3];
+arr = [true,false,true];
+arr = ["Ich","bin","Max","Mütze"];
+
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1. Position im Array)
+output(arr[3]); // letzte Position konkret
+output(arr[arr.length -1]); // letzte Position allgemein
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
