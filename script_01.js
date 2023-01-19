53171394c762710eebd61fe61c02af070960eae8
Marinus <marinus.fehling@web.de>
2023-01-19 10:57:56 +0100

    1. Iteration

--- script_01.js
+++ script_01.js
@@ -4,11 +4,24 @@
 
 /*** 02. Theorie: Array */
 
-
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 
+output(getSentence("Ich","bin","Max","Mütze"));
 
+function getSentence(word1, word2, word3, word4)
+{
+    const GAP = " ";
+    const PUNCT = ".";
+
+    let str = word1 + GAP +
+              word2 + GAP +
+              word3 + GAP +
+              word4 +
+              PUNCT;
+              
+    return str;
+}
 
 
 
